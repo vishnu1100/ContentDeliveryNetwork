68de118392f66eac0fc2dc001f1e6e9b5657741d
vishnu1100 <vishnusanthoshvr@gmail.com>
2024-12-10 06:45:42 +0530

    `Deleted Test.html and free-register-login-API-V2.js files`

---
+++ js-resources/OpenWidget.js
@@ -0,0 +1,8 @@
+
+
+  window.__ow = window.__ow || {};
+  window.__ow.organizationId = "db421598-1431-4eb3-adb8-f312586bfca4";
+  window.__ow.integration_name = "manual_settings";
+  window.__ow.product_name = "openwidget";   
+  ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))
+
